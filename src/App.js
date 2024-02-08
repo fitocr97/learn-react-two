1f18ce77b9f44d4342087d9a88335befff436664
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-08 09:03:08 -0600

    anatomia componente basado en clases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//antomia de un componente basado en clases en react
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{ //app extiende clase incluida en react  
+  render(){ //render retornar siempre va estar cuando cree un componente
+    return(
+      <div>
+        <p>Hola</p>
+        <button onClick={ ()=> console.log('click')}>Enviar</button>
+      </div>
+    )
+  }
 }
+
 
 export default App;
